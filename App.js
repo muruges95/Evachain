fe8dac550b673ef9d6869aab29f6cc8583fc2c30
Ethan Chen <ethanchen@Ethans-MacBook-Pro.local>
2018-09-03 19:33:48 -0700

    Add directions package

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { MapView } from "expo";
+
+// documentation link: https://github.com/bramus/react-native-maps-directions
+import MapViewDirections from 'react-native-maps-directions';
+
+
+const origin = {latitude: 37.3318456, longitude: -122.0296002};
+const destination = {latitude: 37.771707, longitude: -122.4053769};
+const origin1 = {latitude: 37.4512637, longitude: -122.18994862};
+const destination1 = {latitude: 37.4400225, longitude: -122.1603};
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBxkd_k7Aw4qDZagtS5BDuAxdNS6EZbues';
+
+
+
 export default class App extends React.Component {
   render() {
     return (
-      <MapView
-        style={{
-          flex: 1
-        }}
+      <MapView 
+        style={{flex:1}}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 37.3318456, 
+          longitude: -122.0296002,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
-        }}
-      />
-    );
+        }}>
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="red"
+        />
+        <MapViewDirections
+          origin={origin1}
+          destination={destination1}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="green"
+        />
+      </MapView>
+    )
   }
 }
