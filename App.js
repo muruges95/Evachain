64ca177785d3f722723ed9ed5168d13bc221e2b2
Muruges <muruges95@hotmail.com>
2018-09-05 01:02:06 -0700

    added framework for 3 basic screens

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { MapView } from "expo";
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 
 import DashboardScreen from "./screens/DashboardScreen";
@@ -8,44 +5,10 @@ import ListMapScreen from "./screens/ListMapScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 
 
-const DashboardStack = createStackNavigator({
-  Dashboard: DashboardScreen
-});
-
-const ListMapStack = createStackNavigator({
-  ListMap: ListMapScreen
-});
-
-const ProfileStack = createStackNavigator({
-  Profile: ProfileScreen
-});
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-        <MapView
-          style={{
-            flex: 1
-          }}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }}
-        />
-    );
+export default createBottomTabNavigator(
+  {
+    Dashboard: DashboardScreen,
+    ListMap: ListMapScreen,
+    Profile: ProfileScreen
   }
-}
-
-// export default createBottomTabNavigator({
-//   Home: {
-//     screen: HomeScreen
-//   },
-// });
-
-export default createBottomTabNavigator({
-  DashboardStack,
-  ListMapStack,
-  ProfileStack
-});
+);
--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import { MapView } from "expo";
-
-import { StyleSheet } from 'react-native';
+import { Text, View } from "react-native";
 
 export default class DashboardScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Dashboard'
-  };
-
-  render() {
+	render() {
 		return (
-				<MapView
-					style={{
-						flex: 1
-					}}
-					initialRegion={{
-						latitude: 37.78825,
-						longitude: -122.4324,
-						latitudeDelta: 0.0922,
-						longitudeDelta: 0.0421
-				}}
-			/>
+			<View>
+				<Text>Dashboard here.</Text>
+			</View>
 		)
 	}
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		paddingTop: 15,
-		backgroundColor: '#fff',
-	},
-});
