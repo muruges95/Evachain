a018713e0f767025043ca664670df74af4491b23
Cherry Wee Li Chern <cherryweelichern@gmail.com>
2018-09-08 14:11:29 -0700

    adding just words cuz imma noob stop judging me

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { Text, View } from "react-native";
+import React, {Component} from 'react';
+import { Text, StyleSheet, View } from "react-native";
+
+
 
 export default class ProfileScreen extends React.Component {
 	render() {
 		return (
 			<View>
-				<Text>Profile page here.</Text>
+				<Text style={styles.black}> Name here</Text>
+				<Text style={styles.red}>View and edit profile</Text>
+				<Text style={styles.black}> Location Settings</Text>
+				<Text style={styles.black}> Notifications</Text>
+				<Text style={styles.black}> FAQ </Text>
+				<Text style={styles.black}> Give us feedback </Text>
+
 			</View>
-		)
+		);
 	}
 }
+
+const styles = StyleSheet.create({
+  black: {
+    color: 'black',
+    fontSize: 20,
+  },
+  red: {
+    color: 'red',
+    fontSize: 15
+  },
+});
+
