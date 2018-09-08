f20ec12db049c349247459457be1ef864e98cedf
demitycho <tanweihao94@gmail.com>
2018-09-08 11:03:01 -0700

    Write connectors for databases

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import { Text, View } from "react-native";
+import {retrieveData, postData, toJson} from "../api/db.js";
 
 export default class DashboardScreen extends React.Component {
+	constructor(props){
+	    super(props);
+	    this.state = {
+	        data: null
+        };
+    }
+	componentDidMount() {
+		// var wtv = retrieveData('testDB1');
+		// this.setState({data: wtv});
+		// console.log(this.state.data);
+	}
+	getFromDB = () => {
+        retrieveData('testdb1').then(response => {
+            // console.log("Response: ",response);
+            this.setState({data: response});
+            return response;
+
+        });
+        console.log("data",toJson(this.state.data, ["age","gender","height","name"]));
+    }
+	postToDB = () => {
+        const someShit = {
+            name: "weihao",
+            age: 20,
+            gender: "Male",
+            height: 100
+
+        };
+        postData(someShit, 'testdb1');
+    }
+
 	render() {
-		return (
+
+	    return (
 			<View>
 				<Text>Dashboard here.</Text>
+                <Text>{this.state.data? this.state.data.toString():"Haven't load"}</Text>
+                <Text onPress={this.getFromDB}>Get Data</Text>
+                <Text>Dashboard here.</Text>
+                <Text>Dashboard here.</Text>
+                <Text>Dashboard here.</Text>
+                <Text>Dashboard here.</Text>
+                <Text onPress={this.postToDB}>Post Data</Text>
 			</View>
 		)
 	}
